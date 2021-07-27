356f45fc67e58917a03b3b9bbb351d7048647de6
Vladlena Popovici <vladapopovici00@gmail.com>
2021-07-27 18:59:40 +0300

    Added GeneratedValue stategy for primary key

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -1,9 +1,15 @@
 package entities;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
@@ -16,6 +22,7 @@ import java.time.LocalDate;
 public class Car {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String model;
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,6 @@ import entities.Car;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.time.LocalDate;
 
 public class Main {
 
@@ -13,25 +12,13 @@ public class Main {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        Car car1 = new Car();
-        car1.setId(1);
-        car1.setModel("Audi");
-        car1.setColor("white");
-        car1.setMileage(500_000);
-        car1.setProdYear(LocalDate.of(2005, 10, 1));
-
-        Car car2 = Car.builder()
-                .id(2)
-                .model("Porsche")
-                .color("Black")
-                .mileage(100_000)
-                .prodYear(LocalDate.of(2021, 1, 30))
+        Car car1 = Car.builder()
+                .model("Lada")
                 .build();
 
         entityManager.getTransaction().begin();
 
         entityManager.persist(car1);
-        entityManager.persist(car2);
 
         entityManager.getTransaction().commit();
     }
